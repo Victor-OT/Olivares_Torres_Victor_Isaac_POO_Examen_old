6018f0fc9527e610827fcab045dd1c3376e31c47
Victor-OT <crowbreaker96@hotmail.com>
2021-07-01 02:21:15 -0500

    Se finaliza ejercicio 5 y con esto se concluye el examen

--- Assets/Scripts/Enemigos/Colision.cs
+++ Assets/Scripts/Enemigos/Colision.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /*
 Nombre completo: Olivares Torres Victor Isaac
 Asignatura: Programacion Orientada a Objetos
-Nombre y número de ejercicio: Ejercicio 4
+Nombre y número de ejercicio: Ejercicio 4 y 5
 Descripción de lo que hace el script: Este script registra las colisiones que pide como
 parametro el nombre del objeto colisionado, además de invocar el metodo de causar daño heredado
 del script DañoJugador.
@@ -26,13 +26,21 @@ public class Colision : MonoBehaviour
     }
 
     /*este metodo registra la colision del jugador con el enemigo, además de
-     causar daño al jugador*/
+     causar daño al jugador, */
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "Enemy2")
         {
             this.gameObject.GetComponent<DañoJugador>().Daño();
             Debug.Log("Me estás tocando");
+        }
+
+        //Aqui registra una colision, a su vez hace una suma al contadorCoin y destruye el objeto Coin
+        if (collision.gameObject.name == "Coin")
+        {
+            Debug.Log("Una Moneda!");
+            this.gameObject.GetComponent<DañoJugador>().SumaCoins();
+            Destroy(collision.gameObject);
         }
     }
 }
--- Assets/Scripts/Enemigos/DañoJugador.cs
+++ Assets/Scripts/Enemigos/DañoJugador.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /*
 Nombre completo: Olivares Torres Victor Isaac
 Asignatura: Programacion Orientada a Objetos
-Nombre y número de ejercicio: Ejercicio 4
+Nombre y número de ejercicio: Ejercicio 4 y 5
 Descripción de lo que hace el script: Este Script se encarga de tener la funcion de causar
 daño al jugador
 */
@@ -13,13 +13,18 @@ daño al jugador
 public class DañoJugador : MonoBehaviour
 {
     //Variable que restara valor al contador de vidas
-    public float daño = 1;
+    float daño = 1;
+
+    int incrementoScore = 1;
+
 
     ContadorVida registroVida;
+    ContadorCoins registroCoins;
     // Start is called before the first frame update
     void Start()
     {
         registroVida = GetComponent<ContadorVida>();
+        registroCoins = GetComponent<ContadorCoins>();
     }
 
     // Update is called once per frame
@@ -38,5 +43,11 @@ public class DañoJugador : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
+
+    //Metodo que suma coins al colisionar con objeto coin
+    public void SumaCoins()
+    {
+        registroCoins.coins += incrementoScore;
     }
 }
